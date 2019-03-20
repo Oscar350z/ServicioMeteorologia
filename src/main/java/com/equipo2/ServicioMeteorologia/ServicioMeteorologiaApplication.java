ca77aaa8a18f6f5d64d8e099de5f33b032b002f4
Oscar350z <oscarrio35@gmail.com>
2019-03-20 17:45:10 +0100

    primer commit

---
+++ src/main/java/com/equipo2/ServicioMeteorologia/ServicioMeteorologiaApplication.java
@@ -0,0 +1,13 @@
+package com.equipo2.ServicioMeteorologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioMeteorologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioMeteorologiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipo2/ServicioMeteorologia/ServicioMeteorologiaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.equipo2.ServicioMeteorologia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServicioMeteorologiaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
